a9eb442dd0dc72e5bda1a6dbf7c4c199ce10db0a
Anton Styazhkin <rudana089@gmail.com>
2023-03-30 21:02:16 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -6,10 +6,7 @@ function fetchCountries(name) {
 
   return fetch(url).then(response => {
     if (!response.ok) {
-      if (response.status == 404) {
-        throw Notify.failure(`Oops, there is no country with that name`);
-      }
-      throw Notify.failure(`Error! Status ${response.status}`);
+      throw Notify.failure(`Oops, there is no country with that name`);
     }
     return response.json();
   });
