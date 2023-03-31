e8e0e5fd39a5155b5303c23a98e2243294de16d1
Anton Styazhkin <rudana089@gmail.com>
2023-03-31 15:39:06 +0300

    Add searchBox.addEventListener

--- src/index.js
+++ src/index.js
@@ -8,3 +8,5 @@ const DEBOUNCE_DELAY = 300;
 const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
+
+searchBox.addEventListener('input', handleSearchDebounced);
